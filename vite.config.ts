0b74675afa618290519db1276d08abf3513dc54f
vadthyadevendar <vadthyadevendar748@gmail.com>
2025-08-03 18:54:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/digital-lift-spark/",
+
   server: {
     host: true,         // Enables access via network (IPv4 and IPv6)
     port: 8080,
