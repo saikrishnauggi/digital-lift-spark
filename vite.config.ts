1e52ac5b036dd6d11351a6951953299f75de922a
saikrishnauggi <saikrishnauggi@gmail.com>
2025-08-04 22:08:32 +0530

    normal commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/digital-lift-spark/",
-
   server: {
-    host: true,         // Enables access via network (IPv4 and IPv6)
+    host: "::",
     port: 8080,
+
   },
   plugins: [
     react(),
-    ...(mode === "development" ? [componentTagger()] : []), // Cleaner plugin condition
-  ],
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
